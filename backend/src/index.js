6de08bf5ed49ebaec815e8eb05d828d0e0146e79
Bruno <bruno.sac.com.br>
2020-01-24 15:57:02 -0300

    commands

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,5 @@
+/*
+const process = require('process');
 const express = require('express')
 const mongose = require('mongoose')
 const routes = require('./routes')
@@ -16,4 +18,34 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333)
+//app.listen(3333)
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+module.exports = app;
+*/
+'use strict';
+
+// [START gae_node_request_example]
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send('Hello, world!')
+    .end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
